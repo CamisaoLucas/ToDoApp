81e34c7f4c4bb112a7a3e56b8ab4f2986a832a47
Camisao.Lucas <lcscms326@gmail.com>
2021-07-12 19:16:30 -0300

    adicionando models

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+//Imports
 const express = require('express');
+
+
+//configurações
+const app = express();
+const port = 5555
+
+
+//Import Routers
 const usuarioController = require('./controller/usuario-controller');
 const tarefaController = require('./controller/tarefa-controller');
 
-const port = 5555
 
-const app = express();
+//Models 
+const User = require('./models/UserModel')
+const Tarefa = require('./models/TarefaModel')
 
+
+//
+app.use(express.json())
+
+
+//Rotas
 usuarioController(app);
 tarefaController(app);
 
+
+//Listen
 app.listen(port, ()=>console.log(`[INFO]: Servidor rodando em localhost: ${port}`));
+
+console.log(new Tarefa('Resilia','Description','Ativa', '25/03/21'))
